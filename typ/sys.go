d626eebf0e54a14012fbb7f973759500df2d31c7
Martin Schnabel <mb0@mb0.org>
2021-08-01 23:43:18 +0200

    typ: add type system with some type unification support

---
+++ typ/sys.go
@@ -0,0 +1,279 @@
+package typ
+
+import (
+	"fmt"
+
+	"xelf.org/xelf/knd"
+)
+
+// Reg is the type registry interface used to resolve type references.
+// The Reg of package lit is the only noteworthy implementation.
+type Reg interface{ RefType(string) (Type, error) }
+
+// Sys is the resolution context used to instantiate, bind, update and unify types.
+// Type unification is part of sys, because it needs close access to the type variable bindings.
+type Sys struct {
+	MaxID int32
+	Map   map[int32]Type
+	Reg   Reg
+}
+
+// NewSys creates and returns an empty type system using the given type registry.
+func NewSys(reg Reg) *Sys {
+	return &Sys{Map: make(map[int32]Type), Reg: reg}
+}
+
+// Bind binds type t using an existing id or sets a new id and returns the update type.
+func (sys *Sys) Bind(t Type) Type {
+	if t.ID <= 0 {
+		sys.MaxID++
+		t.ID = sys.MaxID
+	}
+	sys.Map[t.ID] = t
+	return t
+}
+
+// Get returns the bound type for id or void.
+func (sys *Sys) Get(id int32) Type {
+	if id <= 0 {
+		return Void
+	}
+	return sys.Map[id]
+}
+
+// Update updates all vars and refs in t with the currently bound types or returns an error.
+func (sys *Sys) Update(t Type) (Type, error) { return Edit(t, sys.update) }
+func (sys *Sys) update(e *Editor) (Type, error) {
+	if e.Kind&knd.Var != 0 {
+		r := sys.Get(e.ID)
+		if r != Void {
+			return r, nil
+		}
+	}
+	if e.Kind&knd.Ref != 0 {
+		b, ok := e.Type.Body.(*RefBody)
+		if ok {
+			r, err := sys.Reg.RefType(b.Ref)
+			if err != nil {
+				return e.Type, nil
+			}
+			if e.Kind&knd.None != 0 {
+				r.Kind |= knd.None
+			}
+			return r, nil
+		}
+	}
+	return e.Type, nil
+}
+
+// Inst instantiates all vars in t for sys and returns the result or an error.
+func (sys *Sys) Inst(t Type) (Type, error) { return sys.inst(t, make(map[int32]Type)) }
+func (sys *Sys) inst(t Type, m map[int32]Type) (Type, error) {
+	return Copy(t, func(e *Editor) (Type, error) {
+		r := e.Type
+		if r.ID > 0 {
+			if r, ok := m[r.ID]; ok {
+				return r, nil
+			}
+		}
+		old := r.ID
+		if r.ID != 0 || r.Kind&knd.Var != 0 {
+			sys.MaxID++
+			r.ID = sys.MaxID
+		}
+		switch b := r.Body.(type) {
+		case *RefBody:
+			if sys.Reg != nil {
+				n, err := sys.Reg.RefType(b.Ref)
+				if err != nil {
+					break // ignore error here
+				}
+				if r.Kind&knd.None != 0 {
+					n.Kind |= knd.None
+				}
+				return n, nil
+			}
+		case *SelBody:
+			// TODO resolve path or think about type selections
+			var par *Editor
+			for par = e.Parent; par != nil; par = par.Parent {
+				_, ok := par.Type.Body.(*ParamBody)
+				if ok {
+					break
+				}
+			}
+			return Select(par.Type, b.Path)
+		}
+		if old > 0 {
+			m[old] = r
+		}
+		return r, nil
+	})
+}
+
+// Unify unifies type t and h and returns the result or an error.
+func (sys *Sys) Unify(t, h Type) (Type, error) {
+	n, err := sys.Update(t)
+	if err != nil {
+		return n, err
+	}
+	if h == Void {
+		return n, nil
+	}
+	h, err = sys.Update(h)
+	if err != nil {
+		return t, err
+	}
+	r, err := unify(sys, t, h)
+	if err != nil {
+		return t, err
+	}
+	r = unibind(sys, n, h, r)
+	return r, nil
+}
+
+func unify(sys *Sys, t, h Type) (Type, error) {
+	a := base(t)
+	b := base(h)
+	kk := a.Kind | b.Kind
+	if kk&(knd.Sel|knd.Ref) != 0 {
+		return Void, fmt.Errorf("cannot unify sel or ref %s with %s", t, h)
+	}
+	r := a
+	ak := a.Kind &^ (knd.Var | knd.None)
+	bk := b.Kind &^ (knd.Var | knd.None)
+	if ak == 0 {
+		if a.ID == 0 {
+			return unibind(sys, a, Void, b), nil
+		}
+		r.Kind = b.Kind
+		if b.Kind == 0 || b.Kind.IsAlt() {
+			r.Kind |= knd.Var
+		}
+		r.Body = b.Body
+		return unibind(sys, a, b, r), nil
+	}
+	if bk == 0 {
+		return unibind(sys, Void, b, a), nil
+	}
+	if kk&knd.Alt != 0 {
+		x, y := a, b
+		if ak&knd.Alt == 0 {
+			x, y = b, a
+		}
+		// TODO merge alts
+		_, _ = x, y
+	}
+	if ak == bk {
+		if equalBody(a.Body, b.Body) {
+			return unibind(sys, Void, b, a), nil
+		}
+		switch ab := a.Body.(type) {
+		case *ElBody:
+			bb, ok := b.Body.(*ElBody)
+			if !ok {
+				return a, nil
+			}
+			el, err := sys.Unify(ab.El, bb.El)
+			if err != nil {
+				return Void, err
+			}
+			r.Body = &ElBody{El: el}
+			return unibind(sys, a, b, r), nil
+		}
+	} else {
+		k := a.Kind & knd.Any
+		if k == 0 {
+			k = b.Kind & knd.Any
+		} else {
+			k = k & b.Kind
+		}
+		if k != 0 {
+			if equalBody(a.Body, b.Body) {
+				r.Kind = k
+				return unibind(sys, a, b, r), nil
+			}
+			if a.Body == nil {
+				r.Kind = k
+				r.Body = b.Body
+				return unibind(sys, a, b, r), nil
+			}
+			if b.Body == nil {
+				r.Kind = k
+				return unibind(sys, a, b, r), nil
+			}
+		}
+	}
+	return Void, fmt.Errorf("cannot unify %s with %s", t, h)
+}
+
+func equalBody(a, b Body) bool {
+	if a == nil {
+		return b == nil
+	}
+	return a.Equal(b)
+}
+
+func base(t Type) Type {
+	for t.Kind&knd.Exp != 0 && t.Kind&knd.Exp != knd.Typ {
+		b, ok := t.Body.(*ElBody)
+		if ok && b.El != Void {
+			t = b.El
+		} else {
+			t = Any
+		}
+	}
+	return t
+}
+
+func unibind(sys *Sys, a, b, r Type) Type {
+	if a.Kind&knd.Var != 0 {
+		if r.ID <= 0 {
+			r.ID = a.ID
+		}
+		sys.Map[a.ID] = r
+	}
+	if b.Kind&knd.Var != 0 {
+		if r.ID <= 0 {
+			r.ID = b.ID
+		}
+		sys.Map[b.ID] = r
+	}
+	if r.ID > 0 {
+		sys.Map[r.ID] = r
+	}
+	return r
+}
+
+// Free appends all unbound type variables in t to res and returns the result.
+func (c *Sys) Free(t Type, res []Type) []Type {
+	if t.Kind&(knd.Var|knd.Ref|knd.Sel) != 0 {
+		if t.ID > 0 {
+			// skip if we already collect this type
+			for _, r := range res {
+				if r.ID == t.ID {
+					return res
+				}
+			}
+			// update from context
+			t, _ = c.Update(t)
+		}
+		// append if still a free type
+		if t.Kind&(knd.Var|knd.Ref|knd.Sel) != 0 {
+			res = append(res, t)
+		}
+	}
+	switch b := t.Body.(type) {
+	case *ElBody:
+		res = c.Free(b.El, res)
+	case *AltBody:
+		for _, a := range b.Alts {
+			res = c.Free(a, res)
+		}
+	case *ParamBody:
+		for _, p := range b.Params {
+			res = c.Free(p.Type, res)
+		}
+	}
+	return res
+}
---
+++ typ/sys_test.go
@@ -0,0 +1,133 @@
+package typ
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestCtx(t *testing.T) {
+	s := Func("",
+		P("", Func("", P("", Var(1, Void)), P("", Bool))),
+		P("", ListOf(Var(1, Void))),
+		P("", Sel(".1")),
+	)
+	want := `<func <func @1 bool> list|@1 .1>`
+	if got := s.String(); got != want {
+		t.Errorf("want %s\ngot %s", want, got)
+	}
+	sys := NewSys(nil)
+	sys.MaxID = 5
+	s, err := sys.Inst(s)
+	if err != nil {
+		t.Errorf("inst want %s got err %v", want, err)
+	}
+	want = `<func <func @6 bool> list|@6 list|@6>`
+	if got := s.String(); got != want {
+		t.Errorf("want inst %s\ngot %s", want, got)
+	}
+	free := sys.Free(s, nil)
+	if len(free) != 1 || free[0].ID != 6 {
+		t.Errorf("want free [@6] got %s", free)
+	}
+	sys.Bind(WithID(6, Int))
+	free = sys.Free(s, nil)
+	if len(free) != 0 {
+		t.Errorf("want free [] got %s", free)
+	}
+	a, _ := sys.Update(s)
+	want = `<func <func int bool> list|int list|int>`
+	if got := a.String(); got != want {
+		t.Errorf("want %s\ngot %s", want, got)
+	}
+}
+
+func TestUnify(t *testing.T) {
+	tests := []struct {
+		a, b string
+		err  string
+		w    Type
+	}{
+		{"int", "int", "", Int},
+		{"typ", "typ", "", Typ},
+		{"num", "int", "", Int},
+		{"int", "num", "", Int},
+		{"num", "num", "", Num},
+		{"num", "any", "", Num},
+		{"int", "real", "cannot", Void},
+		{"num", "str", "cannot", Void},
+		{"num", "<alt int str>", "", Int},
+		{"<alt real bits>", "<alt int str>", "cannot", Void},
+		{"<alt real bits str>", "<alt int str>", "", Str},
+		{"<alt@ real bits str>", "<alt int str>", "", WithID(1, Str)},
+		{"char", "<alt str cont>", "", Str},
+		{"@", "num", "", Var(1, Num)},
+		{"@", "int", "", WithID(1, Int)},
+		{"int", "@", "", WithID(1, Int)},
+		{"@", "@", "", Var(1, Void)},
+		{"list|@", "list|int", "", ListOf(WithID(1, Int))},
+		{"list|str", "list|int", "cannot", Void},
+		{"<rec x:int y:int>", "<rec x:int y:int>", "", Rec(P("x", Int), P("y", Int))},
+		{"<rec x:int y:int>", "any", "", Rec(P("x", Int), P("y", Int))},
+	}
+	for _, test := range tests {
+		a, err := Parse(test.a)
+		if err != nil {
+			t.Errorf("read %s error: %v", test.a, err)
+			continue
+		}
+		b, err := Parse(test.b)
+		if err != nil {
+			t.Errorf("read %s error: %v", test.b, err)
+			continue
+		}
+		sys := NewSys(nil)
+		a, _ = sys.Inst(a)
+		b, _ = sys.Inst(b)
+		r, err := sys.Unify(a, b)
+		if err != nil {
+			if test.err == "" {
+				t.Errorf("unify ab error for %s %s: %v", r, test.b, err)
+				continue
+			} else if !strings.Contains(err.Error(), test.err) {
+				t.Errorf("unify ab want error %s for %s %s got: %v",
+					test.err, test.a, test.b, err)
+			}
+		} else if test.err != "" {
+			t.Errorf("unify ab want error %s for %s %s got none",
+				test.err, test.a, test.b)
+		}
+		if test.err == "" && !r.Equal(test.w) {
+			t.Errorf("unify ab for %s %s want %s got %s\n",
+				a, b, test.w, r)
+		}
+	}
+}
+
+func TestUnifyError(t *testing.T) {
+	tests := []struct {
+		a, b Type
+	}{
+		{Num, Char},
+		{Var(1, Char), Int},
+		{Int, Var(1, Char)},
+		{Alt(Num, Int), Char},
+		{ListOf(Alt(Num)), ListOf(Char)},
+	}
+	for _, test := range tests {
+		sys := NewSys(nil)
+		m := make(map[int32]Type)
+		a, _ := sys.inst(test.a, m)
+		b, _ := sys.inst(test.b, m)
+		r := sys.Bind(Var(0, Void))
+		var err error
+		r, err = sys.Unify(r, a)
+		if err != nil {
+			t.Errorf("unify a error for %s %s: %+v", a, b, err)
+			continue
+		}
+		r, err = sys.Unify(r, b)
+		if err == nil {
+			t.Errorf("unify b want error for %s %s got %s", a, b, r)
+		}
+	}
+}
