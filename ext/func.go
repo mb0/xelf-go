439b12c81a11f54adbd97d030629e9268ec1f746
Martin Schnabel <mb0@mb0.org>
2021-08-05 16:58:14 +0200

    ext: add func spec calling native go functions using reflection

---
+++ ext/func.go
@@ -0,0 +1,130 @@
+// Package ext contains helpers to extend a xelf language with go function specs and node forms.
+package ext
+
+import (
+	"fmt"
+	"reflect"
+
+	"xelf.org/xelf/exp"
+	"xelf.org/xelf/knd"
+	"xelf.org/xelf/lit"
+	"xelf.org/xelf/typ"
+)
+
+// Func is func spec implementation calling an native go function using reflection.
+type Func struct {
+	exp.SpecBase
+	val  reflect.Value
+	rts  []reflect.Type
+	vari bool
+	err  bool
+}
+
+// NewFunc reflects function value val and returns a named func spec or an error.
+// The variadic names parameter can be specified for parameter names that is elided from the
+// reflect function type information.
+func NewFunc(reg *lit.Reg, name string, val interface{}, names ...string) (*Func, error) {
+	v := reflect.ValueOf(val)
+	if v.Kind() != reflect.Func {
+		return nil, fmt.Errorf("expect function argument got %T", val)
+	}
+	if reg == nil {
+		reg = &lit.Reg{}
+	}
+	t := v.Type()
+	n := t.NumIn()
+	pb := &typ.ParamBody{Name: name}
+	s := &Func{SpecBase: exp.SpecBase{Decl: typ.Type{Kind: knd.Func, Body: pb}},
+		val: v, rts: make([]reflect.Type, 0, n), vari: t.IsVariadic(),
+	}
+	for i := 0; i < n; i++ {
+		rt := t.In(i)
+		lt, err := reg.Reflect(rt)
+		if err != nil {
+			return nil, err
+		}
+		var name string
+		if i < len(names) {
+			name = names[i]
+		}
+		s.rts = append(s.rts, rt)
+		pb.Params = append(pb.Params, typ.P(name, lt))
+	}
+	n = t.NumOut()
+	var res typ.Type
+	for i := 0; i < n; i++ {
+		rt := t.Out(i)
+		if rt.ConvertibleTo(refErr) {
+			s.err = true
+			if i+1 < n {
+				return nil, fmt.Errorf("error can only be last result")
+			}
+			break
+		}
+		if i > 0 {
+			return nil, fmt.Errorf("expect at most one result and maybe an error")
+		}
+		lt, err := reg.Reflect(rt)
+		if err != nil {
+			return nil, err
+		}
+		res = lt
+	}
+	pb.Params = append(pb.Params, typ.P("", res))
+	return s, nil
+}
+
+func (s *Func) Value() lit.Val { return s }
+func (s *Func) Eval(p *exp.Prog, c *exp.Call) (*exp.Lit, error) {
+	args, err := p.EvalArgs(c)
+	if err != nil {
+		return nil, err
+	}
+	// get reflect values from argument
+	rvs := make([]reflect.Value, len(s.rts))
+	for i, rt := range s.rts {
+		arg := args[i]
+		if arg == nil || arg.Val == nil || arg.Val.Zero() {
+			ptr := reflect.New(rt)
+			rvs[i] = ptr.Elem()
+			// reflect already provides a zero value
+			continue
+		}
+		val, err := p.Reg.Conv(rt, arg.Val)
+		if err != nil {
+			return nil, err
+		}
+		rvs[i] = val
+	}
+	// call reflect function with value
+	var res []reflect.Value
+	if s.vari {
+		res = s.val.CallSlice(rvs)
+	} else {
+		res = s.val.Call(rvs)
+	}
+	if s.err { // check last result
+		last := res[len(res)-1]
+		if !last.IsNil() {
+			return nil, last.Interface().(error)
+		}
+		res = res[:len(res)-1]
+	}
+	if len(res) == 0 { // nothing to return
+		return nil, nil
+	}
+	rv := res[0]
+	if rv.Type().Kind() != reflect.Ptr {
+		rn := reflect.New(rv.Type())
+		rn.Elem().Set(rv)
+		rv = rn
+	}
+	// create a proxy from the result and return
+	prx, err := p.Reg.ProxyValue(rv)
+	if err != nil {
+		return nil, err
+	}
+	return &exp.Lit{Res: exp.SigRes(c.Sig).Type, Val: prx, Src: c.Src}, nil
+}
+
+var refErr = reflect.TypeOf((*error)(nil)).Elem()
---
+++ ext/func_test.go
@@ -0,0 +1,119 @@
+package ext
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	"xelf.org/xelf/bfr"
+	"xelf.org/xelf/exp"
+	"xelf.org/xelf/lib"
+	"xelf.org/xelf/lit"
+	"xelf.org/xelf/typ"
+)
+
+func TestFunc(t *testing.T) {
+	reg := &lit.Reg{}
+	echo, err := NewFunc(reg, "echo", func(s string) string { return s })
+	if err != nil {
+		t.Fatal("failed to create node spec for test Element")
+	}
+	env := exp.Builtins(make(lib.Specs).AddMap(lib.Core).Add(echo))
+	tests := []struct {
+		raw string
+		xlf string
+	}{
+		{`(echo "a")`, `'a'`},
+	}
+	for _, test := range tests {
+		got, err := exp.Eval(reg, env, test.raw)
+		if err != nil {
+			t.Errorf("eval %s failed: %v", test.raw, err)
+			continue
+		}
+		vs := bfr.String(got)
+		if vs != test.xlf {
+			t.Errorf("want %s got %s", test.xlf, vs)
+		}
+	}
+}
+
+func TestReflectFunc(t *testing.T) {
+	tests := []struct {
+		fun   interface{}
+		name  string
+		names []string
+		typ   typ.Type
+		err   bool
+	}{
+		{strings.ToLower, "lower", nil, typ.Func("lower",
+			typ.P("", typ.Str),
+			typ.P("", typ.Str),
+		), false},
+		{strings.Split, "", nil, typ.Func("",
+			typ.P("", typ.Str),
+			typ.P("", typ.Str),
+			typ.P("", typ.ListOf(typ.Str)),
+		), false},
+		{time.Parse, "", nil, typ.Func("",
+			typ.P("", typ.Str),
+			typ.P("", typ.Str),
+			typ.P("", typ.Time),
+		), true},
+		{time.Time.Format, "", []string{"t?", "format"}, typ.Func("",
+			typ.P("t?", typ.Time),
+			typ.P("format", typ.Str),
+			typ.P("", typ.Str),
+		), false},
+		{fmt.Sprintf, "", nil, typ.Func("",
+			typ.P("", typ.Str),
+			typ.P("", typ.ListOf(typ.Any)),
+			typ.P("", typ.Str),
+		), false},
+	}
+	for _, test := range tests {
+		f, err := NewFunc(nil, test.name, test.fun, test.names...)
+		if err != nil {
+			t.Errorf("reflect for %+v err: %v", test.fun, err)
+			continue
+		}
+		if !test.typ.Equal(f.Decl) {
+			t.Errorf("for %T want param %s got %s", test.fun, test.typ, f.Decl)
+		}
+		if test.err != f.err {
+			t.Errorf("for %T want err %v got %v", test.fun, test.err, f.err)
+		}
+	}
+}
+
+func TestFuncEval(t *testing.T) {
+	tests := []struct {
+		fun   interface{}
+		names []string
+		raw   string
+		xlf   string
+	}{
+		{strings.ToLower, nil, `(_ 'HELLO')`, `'hello'`},
+		{time.Time.Format, []string{"t?", "format"}, `(_ format:'2006')`, `'0001'`},
+		{fmt.Sprintf, nil, `(_ "Hi %s no. %d." "you" 9)`, `'Hi you no. 9.'`},
+	}
+	for _, test := range tests {
+		reg := &lit.Reg{}
+		f, err := NewFunc(reg, "_", test.fun, test.names...)
+		if err != nil {
+			t.Errorf("reflect for %+v err: %v", test.fun, err)
+			continue
+		}
+		env := exp.Builtins(lib.Specs{"_": f}.AddMap(lib.Core))
+		got, err := exp.Eval(reg, env, test.raw)
+		if err != nil {
+			t.Errorf("eval %s failed: %v", test.raw, err)
+			continue
+		}
+		vs := bfr.String(got)
+		if vs != test.xlf {
+			t.Errorf("want %s got %s", test.xlf, vs)
+		}
+	}
+}
