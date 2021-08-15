a4d76735de339e715487acfbe684c5853c88c2f3
Martin Schnabel <mb0@mb0.org>
2021-08-15 17:43:25 +0200

    lit: more syntax finding considerations for delta sub edits

--- lit/listop_test.go
+++ lit/listop_test.go
@@ -6,6 +6,9 @@ import "testing"
 // I normally edit to pieces and discard as informational test.
 // This kind of information does not really fit into doc or commit comments.
 func TestListOpSyntax(t *testing.T) {
+	if testing.Short() || !testing.Verbose() { // this not ready running any code so leave it
+		return
+	}
 	isItGoodYet := false
 
 	// We already have selection paths '.a/b.-1.5' and we have set and create path that
@@ -83,16 +86,53 @@ func TestListOpSyntax(t *testing.T) {
 	}
 
 	doesItHandleNestedEdits := false
+	// We want to allow nested edits. This would make it more convenient for a human to write
+	// deltas that have multiple edit paths into the same keyr and also easier for the computer
+	// to avoid duplicate field lookups.
+	t.Logf("With {a:{b:[1 2] c:{x:3 y:4} d:5}}")
 
+	// We can already set a path to an object with this syntax:
+	t.Logf("{'.a.c':{z:5}} results in {a:{b:[3 2] c:{z:5} d:5}}")
+
+	// We do not however want to assign but instead edit the object.
+	// We would repeat the path prefix for complex edits:
+	t.Logf("{'.a.b.0':3 '.a.d':4} results in {a:{b:[3 2] c:{x:3 y:4} d:4}}")
+
+	// To indicate a sub edits we need a prefix to indicate that we want to edit with and not
+	// set to the following value, just like with list ops. That said we should use the same
+	// suffix as for list edits.
+	t.Logf("{'.a*':{'.b.0':3 d:4}} results in {a:{b:[3 2] c:{x:3 y:4} d:4}}")
+
+	// With sub edits and the fact that each keyer by itself is also valid edit to itself.
+	// We can use that syntax to merge keyrs.
+	t.Logf("{'.a.c*':{x:1 z:2}} {a:{b:[1 2] c:{x:1 y:4 z:2} d:4}}")
+
+	// So we have the star suffix followed by a list as listop edit and followed by a keyr
+	// as sub edit. We have the minus suffix to delete list ranges and to delete keyr fields.
+	// But lists have the plus suffix for appends, that is not currently mapped to keyrs.
+	// We could leave it at that, make it also indicate a sub edit or come up with a difference
+	// variant of sub edits. List append is always followed by a list so we could easily discern
+	// both syntax variants. But what would be similar to appending for a keyr?
+	// It could mean do not change non nil fields, but this is unintuitive and hard to use.
+	// Or maybe only make a distinction only for dicts to not replace existing value but rather
+	// delete them and append key and new value at the end:
+	t.Logf("{'.a.c+':{x:1 z:2}} {a:{b:[1 2] c:{y:4 x:1 z:2} d:4}}")
+
+	// We also want to allow sub edits in list ops.
 	t.Logf("With {a:[[1 2] [3 4] 5]}")
+
 	// we can start by creating edits for the single replacements we already detect.
 	t.Logf("{'.a.0.0':3} sets the field {a:[[3 2] [2 4] 5}")
 
-	// if we have only matching inserts and deletions (replacements)
-	// we can form idx paths for all individual elements and create a delta
+	// If we have only matching inserts and deletions (replacements) we can form idx paths for
+	// all individual elements and create a delta.
 	t.Logf("{'.a.0.0':3 .a.1.1:7} sets the field {a:[[3 2] [2 7] 5}")
+
+	// We could use a sub edit for the common prefix.
+	t.Logf("{'.a.*':{.0.0':3 .1.1:7}} sets the field {a:[[3 2] [2 7] 5}")
+
 	// we could also check all neighbouring inserts and deletions for overlaps but this
-	// is soon going to spiral out of control down this path.
+	// is soon going to spiral out of control.
 
 	if !doesItHandleNestedEdits {
 		// maybe we just leave it at that?
