3673e25edad34a6a6e8e4ab466410d09590d9e50
Martin Schnabel <mb0@mb0.org>
2022-11-25 21:01:20 +0100

    exp: remove unused select functions on lit for now
    We might want to think about select functions for expressions - unsure what
    that would look like.

--- exp/path.go
+++ exp/path.go
@@ -6,28 +6,8 @@ import (
 	"xelf.org/xelf/typ"
 )
 
-// Select reads path and returns the selected literal from l or an error.
-func Select(l *Lit, path string) (*Lit, error) {
-	p, err := cor.ParsePath(path)
-	if err != nil {
-		return nil, err
-	}
-	return SelectPath(l, p)
-}
-
-// SelectPath returns the selected literal at path from l or an error.
-func SelectPath(l *Lit, path cor.Path) (_ *Lit, err error) {
-	t, err := typ.SelectPath(l.Res, path)
-	if err != nil {
-		return nil, err
-	}
-	v, err := lit.SelectPath(l.Val, path)
-	if err != nil {
-		return nil, err
-	}
-	return &Lit{Res: t, Val: v}, nil
-}
-
+// Select lookup reads path and returns the selected literal from l or an error.
+// If eval is false we attempt to at least resolve the intended type if no value was found.
 func SelectLookup(l *Lit, k string, eval bool) (*Lit, error) {
 	if l == nil || eval && l.Val == nil {
 		return nil, ErrSymNotFound
